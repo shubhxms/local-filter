c4fd43f5382d2f0923622189bb578f3ccf6cd05b
Shubham Shah <03.shubhamshah@gmail.com>
2026-09-17 23:35:14 +0530

    reorganize layout: popup/ and options/ dirs, vendored mark.js into libs/

--- popup.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById('extractBtn').addEventListener('click', async () => {
-  const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
-  const { topics = [] } = await chrome.storage.sync.get('topics');
-  chrome.tabs.sendMessage(tab.id, { topics });
-});
